0432e955047605542e152ee21ef230d01040eb7a
Derek Nutile <dereknutile@gmail.com>
2024-12-01 06:59:24 -0800

    Create auth scaffolding

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,13 +3,17 @@
 namespace App\Http\Controllers;
 
 use Api\Traits\ApiResponses;
-use Illuminate\Http\Request;
+use App\Http\Requests\ApiLoginRequest;
 
 class AuthController extends Controller
 {
     use ApiResponses;
 
-    public function login() {
-        return $this->ok('Hello, login route!');
+    public function login(ApiLoginRequest $request) {
+        return $this->ok($request->get('email'));
+    }
+
+    public function register() {
+        return $this->ok('register');
     }
 }
